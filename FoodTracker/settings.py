73de83b6dbd201508385547a6e6a1f83045b5516
ticitaca-shubham <mastershubh06@gmail.com>
2023-07-02 13:49:50 +0530

    second commit

--- FoodTracker/settings.py
+++ FoodTracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6j5w(7gr8ud^4$8n3f-uos9fx&@k8h(v0w$8kt3y7)!j#a3tiu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
